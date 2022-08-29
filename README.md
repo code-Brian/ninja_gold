f8cfc4908cc86c5f7aa590587c252d3478f6be09
Brian Denmark <testingarea479@gmail.com>
2022-08-28 17:14:02 -0700

    updated project to add to journal for each activity and is color coded

--- README.md
+++ README.md
@@ -9,8 +9,8 @@ This is the optional ninja gold assignment for coding dojo.
 ✔️ 2 Create the template as shown in the wireframe 
 ✔️ 3 Have the root route render the page
 ✔️ 4 have the "/process_money" POST route increase/decrease the user's gold by an appropriate amount and redirect to the root route
-❌ 5 NINJA Bonus: Display all the activities performed by the user in a log on the HTML
-❌ 6 NINJA Bonus: Color-code the activities text
+✔️ 5 NINJA Bonus: Display all the activities performed by the user in a log on the HTML
+✔️ 6 NINJA Bonus: Color-code the activities text
     example: gold gained - green text, gold lost - red text
 ❌ 7 NINJA Bonus: Add a reset button to restart the game
 ❌ 8 SENSEI Bonus: Have the activities display in descending order, with the most recent activity first
--- server.py
+++ server.py
@@ -8,6 +8,9 @@ app.secret_key ='1234567890'
 def index():
     if 'balance' not in session:
         session['balance'] = 0
+    if 'journal' not in session:
+        session['journal'] = []
+        session['journal'].append('<li class="list-group-item">Welcome to Ninja Gold. Vist a location to earn some gold!</li>')
     print("here's the index file!")
     return render_template('index.html')
 
@@ -15,21 +18,31 @@ def index():
 def process_money():
     if request.form['hidden'] == 'farm':
         random = randint(10,20)
+        message = f"<li class='list-group-item bg-success'> Added {random} gold to purse.</li>"
         session['balance'] += random
+        session['journal'].append(message)
     elif request.form['hidden'] == 'cave':
         random = randint(5,10)
+        message = f"<li class='list-group-item bg-success'> Added {random} gold to purse.</li>"
         session['balance'] += random
+        session['journal'].append(message)
     elif request.form['hidden'] == 'house':
         random = randint(2,5)
+        message = f"<li class='list-group-item bg-success'> Added {random} gold to purse.</li>"
         session['balance'] += random
+        session['journal'].append(message)
     else:
         random = randint(1,100)
         if random > 50:
+            message = f"<li class='list-group-item bg-success'> Added 50 gold to purse.</li>"
             session['balance'] += 50
+            session['journal'].append(message)
         else:
+            message = f"<li class='list-group-item bg-danger'> Subtracted 50 gold from purse.</li>"
             session['balance'] -= 50
+            session['journal'].append(message)
 
-    return render_template('index.html', balance_on_template = session['balance'])
+    return render_template('index.html', balance_on_template = session['balance'], journal_on_template = session['journal'])
 
 if __name__ == '__main__':
     app.run(debug=True)
